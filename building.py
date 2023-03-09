60141e18d0f3a04a2b3a82ec53539ec50c81cc7d
TGriff03 <tyler@hakuna.co.uk>
2023-03-09 18:52:33 +0000

    When new settlement is created an object of Settlement is now created and added to list existing_settlements.

--- building.py
+++ building.py
@@ -1,8 +1,22 @@
+import pygame
+from settings import *
+
+
 class Settlement:
-    def __init__(self):
+
+    def __init__(self, player, location):
+        self.player = player
+        self.location = location
+
+    def get_resources(self):
         pass
 
 
 class City:
+    def __init__(self):
+        pass
+
+
+class Development:
     def __init__(self):
         pass
