0270b5823dd74a341d3d9a6a6bf5c853d4373735
rs754 <rob.scott2003@outlook,com>
2023-04-26 11:52:43 +0100

    dice

--- src/dice.py
+++ src/dice.py
@@ -1,9 +1,37 @@
 import random
+import pygame
+from settings import *
 
 
-def __init__(self):
-    self.numbers = [1, 2, 3, 4, 5, 6]
+class Dice:
 
+    def __init__(self):
+        self.numbers = [1, 2, 3, 4, 5, 6]
+        self.dice_images = [
+            pygame.image.load("../resources/dice1.png"),
+            pygame.image.load("../resources/dice2.png"),
+            pygame.image.load("../resources/dice3.png"),
+            pygame.image.load("../resources/dice4.png"),
+            pygame.image.load("../resources/dice5.png"),
+            pygame.image.load("../resources/dice6.png"),
+        ]
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.dice_rect = pygame.Rect(WIDTH - 170, HEIGHT / 2, 70, 70)
 
-def roll(self):
-    return random.choice(self.numbers)
+    def roll(self):
+        dice_num1 = random.choice(self.numbers)
+        dice_num2 = random.choice(self.numbers)
+        self.draw_dice(dice_num1, dice_num2)
+
+    def draw_dice(self, dice1, dice2):
+        pygame.draw.rect(self.screen, BG_COLOUR, (WIDTH - 170, HEIGHT / 2, 200, 100))
+        dice1_img = pygame.transform.scale(self.dice_images[dice1-1], (70, 70))
+        dice2_img = pygame.transform.scale(self.dice_images[dice2-1], (70, 70))
+        self.screen.blit(dice1_img, (WIDTH - 170, HEIGHT / 2))
+        self.screen.blit(dice2_img, (WIDTH - 100, HEIGHT / 2))
+
+    def draw_default_dice(self):
+        dice1_img = pygame.transform.scale(self.dice_images[5], (70, 70))
+        dice2_img = pygame.transform.scale(self.dice_images[5], (70, 70))
+        self.screen.blit(dice1_img, (WIDTH - 170, HEIGHT / 2))
+        self.screen.blit(dice2_img, (WIDTH - 100, HEIGHT / 2))
